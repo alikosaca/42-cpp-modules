d80704e09ae53efd3eac2dc547bb5e347669235c
alikosaca <alikosaca0@gmail.com>
2026-01-30 15:38:30 +0300

    Add Makefile and megaphone.cpp source file

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(int ac, char *const av[])
+{
+	if (ac > 1)
+	{
+		for (int i = 1; ac > i; i++)
+		{
+			for(int j = 0; av[i][j] != '\0'; j++)
+			{
+				std::cout << static_cast<char>(std::toupper(av[i][j]));
+			}
+		}
+		std::cout << std::endl;
+	}
+	else if (ac  == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return (0);
+}
