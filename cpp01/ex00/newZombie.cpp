e28796ecec3816fcaf4144ce838025b0e2e7ea08
alikosaca <alikosaca0@gmail.com>
2026-02-07 15:54:58 +0300

    Sil: Zombie sınıfı ve ilgili dosyalar kaldırıldı.

--- cpp01/ex00/newZombie.cpp
+++
@@ -1,6 +0,0 @@
-#include "Zombie.hpp"
-
-Zombie *newZombie(std::string name){
-	Zombie *_zombie = new Zombie(name);
-	return _zombie;
-}
--- cpp01/ex01/ZombieHorder.cpp
+++
@@ -1,9 +0,0 @@
-#include "Zombie.hpp"
-
-Zombie *zombieHorder(int N, std::string name)
-{
-	Zombie *zombieHorder = new Zombie[N];
-	for(int i = 0; i < N; i++)
-		zombieHorder[i].setName(name);
-	return zombieHorder;
-}
