e45116a657107f5a908023ab0136fd1f7f5affea
alikosaca <alikosaca0@gmail.com>
2026-02-08 16:39:47 +0300

    feat: implement Harl with switch-based complain system

---
+++ cpp01/ex06/Harl.cpp
@@ -0,0 +1,54 @@
+#include "Harl.hpp"
+
+Harl::Harl(){}
+
+Harl::~Harl(){}
+
+void Harl::debug(void){
+	std::cout << "\e[32m" << "<<DEBUG>> " << "\033[0m";
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info(void){
+	std::cout << "\e[36m" << "<<INFO>> " << "\033[0m";
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning(void){
+	std::cout << "\033[38;5;166m" << "<<WARNING>> " << "\033[0m";
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+}
+
+void Harl::error(void){
+	std::cout << "\033[38;5;196m" << "<<ERROR>> " << "\033[0m";
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain(std::string level){
+	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int index = 0;
+
+	for (int i = 0; i < 4; i++){
+		if (levels[i].compare(level))
+			index++;
+		else
+			break;
+	}
+
+	switch (index) {
+		case 0:
+			this->debug();
+			// fall through
+		case 1:
+			this->info();
+			// fall through
+		case 2:
+			this->warning();
+			// fall through
+		case 3:
+			this->error();
+			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
+}
