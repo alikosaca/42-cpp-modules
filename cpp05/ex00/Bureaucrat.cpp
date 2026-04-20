9188bea8ee59e29737bc175ef14b57b38ec40410
alikosaca <alikosaca0@gmail.com>
2026-04-20 22:09:15 +0300

    fix: correct exception handling in GradeTooHighException and GradeTooLowException methods

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -46,11 +46,11 @@ void Bureaucrat::decrementGrade(){
 }
 
 const char* Bureaucrat::GradeTooHighException::what() const throw(){
-    return "Grade is too hight. it should be in (1-150)";
+    throw "Grade is too hight. it should be in (1-150)";
 }
 
 const char* Bureaucrat::GradeTooLowException::what() const throw(){
-    return "Grade is too low. it should be in (1-150)";
+    throw "Grade is too low. it should be in (1-150)";
 }
 
 
