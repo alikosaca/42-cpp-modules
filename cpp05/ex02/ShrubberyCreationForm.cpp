4b33a593fe95677b41d64b56449ef711578dc8df
alikosaca <alikosaca0@gmail.com>
2026-04-24 20:42:51 +0300

    feat: add ShrubberyCreationForm class with action method for file creation

---
+++ cpp05/ex02/ShrubberyCreationForm.cpp
@@ -0,0 +1,42 @@
+#include "ShrubberyCreationForm.hpp"
+
+#include <fstream>
+
+
+ShrubberyCreationForm::ShrubberyCreationForm() : _target("ShrubberyCreationForm"), AForm("ShrubberyCreationForm", false, 145, 137){}
+
+ShrubberyCreationForm::ShrubberyCreationForm(std::string &target) : _target(target), AForm("ShrubberyCreationForm", false, 145, 137){}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& other) : _target("ShrubberyCreationForm"), AForm(other){}
+
+ShrubberyCreationForm& ShrubberyCreationForm::operator=(const ShrubberyCreationForm& other){
+	if (this != &other){
+		AForm::operator=(other);
+		this->_target = other._target;
+	}
+	return *this;
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm(){}
+
+void ShrubberyCreationForm::action(Bureaucrat const & executor) const{
+    std::ofstream file((_target + "_shrubbery").c_str());
+    if (!file.is_open()) {
+        throw FileNotOpenException();
+    }
+    else {
+        file << "              v .   ._, |_  .," << std::endl;
+        file << "           `-._\\/  .  \\ /    |/_" << std::endl;
+        file << "               \\  \\, y | \\//" << std::endl;
+        file << "         _\\_.___\\, \\/-.\\||" << std::endl;
+        file << "           `7-,--.`._||  / / ," << std::endl;
+        file << "           /'     `-. `./ / |/_." << std::endl;
+        file << "                     |----|//" << std::endl;
+        file << "                     |_    / " << std::endl;
+        file << "     -|||-           |-  _|  " << std::endl;
+        file << "     - | -   _       |   =|  " << std::endl;
+        file << "      -|-   |-|      |    |  " << std::endl;
+        file << "-------|----|-|---|-/ ,  . \\--------._" << std::endl;
+    }
+    file.close();
+}
