4194b60374bcf56569a4c65a5994288a16b485df
alikosaca <alikosaca0@gmail.com>
2026-04-25 21:29:46 +0300

    feat: reorder constructor initialization in RobotomyRequestForm for consistency

--- cpp05/ex02/RobotomyRequestForm.cpp
+++ cpp05/ex02/RobotomyRequestForm.cpp
@@ -3,11 +3,11 @@
 #include <fstream>
 
 
-RobotomyRequestForm::RobotomyRequestForm() : _target("RobotomyRequestForm"), AForm("RobotomyRequestForm", false, 72, 45){}
+RobotomyRequestForm::RobotomyRequestForm() : AForm("RobotomyRequestForm", false, 72, 45), _target("RobotomyRequestForm"){}
 
-RobotomyRequestForm::RobotomyRequestForm(std::string &target) : _target(target), AForm("RobotomyRequestForm", false, 72, 45){}
+RobotomyRequestForm::RobotomyRequestForm(std::string &target) : AForm("RobotomyRequestForm", false, 72, 45), _target(target){}
 
-RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm& other) : _target("RobotomyRequestForm"), AForm(other){}
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm& other) : AForm(other), _target("RobotomyRequestForm"){}
 
 RobotomyRequestForm& RobotomyRequestForm::operator=(const RobotomyRequestForm& other){
 	if (this != &other){
@@ -21,7 +21,7 @@ RobotomyRequestForm::~RobotomyRequestForm(){}
 
 void RobotomyRequestForm::action() const{
     std::cout << "BIIZZZ... VIIIRRR... DIIIRR... *" << std::endl;
-    if (std::rand() % 2) {
+    if (std::rand() % 2 == 0) {
         std::cout << _target << " has been robotomized successfully!" << std::endl;
     } else {
         std::cout << "The robotomy on " << _target << " failed." << std::endl;
