991c444c9bdc827fc2f23bfb13fa7d9463bb8816
alikosaca <alikosaca0@gmail.com>
2026-04-25 22:09:04 +0300

    feat: initialize random seed and enhance main function output in main.cpp

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -1,7 +1,31 @@
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+
+#include <ctime>
 
 int main(void){
+    std::srand(static_cast<unsigned int>(std::time(NULL)));
 
+
+    try{
+        std::cout << "a" << std::endl;
+        Bureaucrat a("Norminette", 42);
+        std::cout << "f" << std::endl;
+        ShrubberyCreationForm f("Home");
+    
+        std::cout << a << std::endl;
+        std::cout << "a yazdık" << std::endl;
+        std::cout << f << std::endl;
+        std::cout << "f yazdık" << std::endl;
+    } catch(std::exception &e){
+        std::cout << "sorun var: " << e.what() << std::endl;
+    }
     return (0);
 }
