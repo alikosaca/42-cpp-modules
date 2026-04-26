b7139b70fc17da46ec9f4915c25fd32718fd7344
alikosaca <alikosaca0@gmail.com>
2026-04-26 16:35:51 +0300

    feat: add PresidentialPardonForm class with action method and constructors

---
+++ cpp05/ex03/PresidentialPardonForm.cpp
@@ -0,0 +1,24 @@
+#include "PresidentialPardonForm.hpp"
+
+#include <fstream>
+
+
+PresidentialPardonForm::PresidentialPardonForm() : AForm("PresidentialPardonForm", false, 25, 5), _target("PresidentialPardonForm"){}
+
+PresidentialPardonForm::PresidentialPardonForm(const std::string &target) : AForm("PresidentialPardonForm", false, 25, 5), _target(target){}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm& other) : AForm(other), _target("PresidentialPardonForm"){}
+
+PresidentialPardonForm& PresidentialPardonForm::operator=(const PresidentialPardonForm& other){
+	if (this != &other){
+		AForm::operator=(other);
+		this->_target = other._target;
+	}
+	return *this;
+}
+
+PresidentialPardonForm::~PresidentialPardonForm(){}
+
+void PresidentialPardonForm::action() const{
+    std::cout << _target << " has been pardoned by Zaphod Beeblebrox." << std::endl;
+}
