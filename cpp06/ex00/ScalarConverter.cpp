97cadeb0f4395c20d8ea7f1ab6659979b176eeae
alikosaca <alikosaca0@gmail.com>
2026-04-26 21:16:33 +0300

    fix: correct logic in pseudoLiterals and convertType functions for better type conversion handling

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -18,10 +18,10 @@ static bool pseudoLiterals(const std::string &literal){
     std::string pseudoLiterals[6] = {"-inff", "+inff", "nanf", "-inf", "+inf", "nan"};
     int i = 0;
 
-    while (literal != pseudoLiterals[i]){
+    while (i < pseudoLiterals->length() && literal != pseudoLiterals[i]){
         i++;
     }
-    if (literal == pseudoLiterals[i]){
+    if (i < pseudoLiterals->length() && literal == pseudoLiterals[i]){
             std::cout << "char: impossible" << std::endl;
             std::cout << "int: impossible" << std::endl;
     	if (literal == "nan" || literal == "nanf"){
@@ -31,13 +31,33 @@ static bool pseudoLiterals(const std::string &literal){
             std::cout << "float: inff" << std::endl;
             std::cout << "double: inf" << std::endl;
         }
-        return (true);
+        return true;
+    }
+}
+
+static bool convertType(const std::string &literal){
+    char* end;
+    double val = std::strtod(literal.c_str(), &end);
+    
+    if (literal.length() == 1 && !isdigit(literal[0])) {
+        double val = static_cast<double>(literal[0]);
+        return true;
+    } else if ((*end != '\0' && !(*end == 'f' && *(end + 1) == '\0')) || (end == literal.c_str())) {
+        std::cout << "char: impossible" << std::endl;
+        std::cout << "int: impossible" << std::endl;
+        std::cout << "float: impossible" << std::endl;
+        std::cout << "double: impossible" << std::endl;
+        return false;
+    } else{
+        return true;
     }
 }
 
 void ScalarConverter::Convert(const std::string &literal){
-    if (pseudoLiterals(literal)){
+    if (!pseudoLiterals(literal)){
         return;
     }
-    
+    if (!convertType(literal)){
+        return;
+    }
 }
