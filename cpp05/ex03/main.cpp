03e2a6f2f218da9fe9279f36b761da41ead72c54
alikosaca <alikosaca0@gmail.com>
2026-05-03 17:00:43 +0300

    fix: refactor form initialization to avoid re-declaration and improve memory management

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -15,50 +15,57 @@
 int main(void)
 {
     std::srand(std::time(NULL));
-
+    AForm* form = NULL;
     try{
         std::cout << "----------------" << std::endl;
         std::cout << "<- | TEST 1 | ->" << std::endl;
         Intern ali;
         Bureaucrat ibrahim("Boss", 1);
-        AForm* form = ali.makeForm("shrubbery creation", "razer");
+        form = ali.makeForm("shrubbery creation", "razer");
         std::cout << ibrahim << std::endl;
         std::cout << *form << std::endl;
 
         ibrahim.signForm(*form);
         ibrahim.executeForm(*form);
         delete form;
+        form = NULL;
     } catch(std::exception &e){
         std::cout << "ups!: " << e.what() << std::endl;
+        delete form;
+        form = NULL;
     }
     try{
         std::cout << "----------------" << std::endl;
         std::cout << "<- | TEST 2 | ->" << std::endl;
         Intern ali;
         Bureaucrat kayhan("junior developer", 150);
-        AForm* form = ali.makeForm("shrubbery creation", "salary");
+        form = ali.makeForm("shrubbery creation", "salary");
 
         kayhan.signForm(*form);
         kayhan.executeForm(*form);
         delete form;
+        form = NULL;
+        std::cout << "aaaaaaaaaaaaaaaaaaaaaaa" << std::endl;
     } catch(std::exception &e){
         std::cout << "ups!: " << e.what() << std::endl;
+        delete form;
+        form = NULL;
     }
     try{
         std::cout << "----------------" << std::endl;
         std::cout << "<- | TEST 3 | ->" << std::endl;
         Intern ali;
         Bureaucrat yunus("product manager", 10);
-        AForm* form = ali.makeForm("begin boss", "monster");
+        form = ali.makeForm("begin boss", "monster");
 
         yunus.signForm(*form);
         yunus.executeForm(*form);
         delete form;
+        form = NULL;
     } catch(std::exception &e){
         std::cout << "ups!: " << e.what() << std::endl;
-    }
-    catch (const std::string& s) {
-        std::cout << s << std::endl;
+        delete form;
+        form = NULL;
     }
 
     return (0);
