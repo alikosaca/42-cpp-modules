54a58a8422b8bcab228d6752f82068983131be4f
alikosaca <alikosaca0@gmail.com>
2026-05-08 21:42:23 +0300

    feat: add main program and utility functions for swapping, min, and max operations

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include "whatever.hpp"
+
+
+int main(void) {
+    int a = 2;
+    int b = 3;
+
+    std::cout << "a = " << a << ", ";
+    std::cout << "b = " << std::endl;
+    ::swap(a, b);
+    std::cout << "a = " << a << ", ";
+    std::cout << "b = " << b << std::endl;
+    std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+    std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+
+    std::cout << "c: " << c << ", ";
+    std::cout << "d: " << d << std::endl;
+    ::swap(c, d);
+    std::cout << "c = " << c << ", ";
+    std::cout << "d = " << d << std::endl;
+    std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
+    std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
+
+    return 0;
+}
