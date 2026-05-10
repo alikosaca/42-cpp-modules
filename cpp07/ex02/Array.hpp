add4fa3f8ad67430e86e4abb771669842e2f8a54
alikosaca <alikosaca0@gmail.com>
2026-05-10 17:29:22 +0300

    feat: add Array class template with basic functionality

---
+++ cpp07/ex02/Array.hpp
@@ -0,0 +1,23 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+template <typename T>
+class Array {
+    private:
+        unsigned int _size;
+        T* _element;
+    public:
+        Array();
+        Array(unsigned int n);
+        Array(const Array& other);
+        Array& operator=(const Array& other);
+        ~Array();
+
+        T& operator[](unsigned int n);
+        const T& operator[](unsigned int n) const;
+        unsigned int size() const;
+};
+
+#include "Array.tpp"
+
+#endif 
