2a93ab77022f1c66edf19552e6ad2afc71ba099b
alikosaca <alikosaca0@gmail.com>
2026-05-30 21:16:58 +0300

    feat: add main function to initialize BitcoinExchange and process input

---
+++ cpp09/ex00/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include "BitcoinExchange.hpp"
+
+int main(int ac, char** av) //int argc, char** argv
+{
+    if (ac != 2){
+        std::cout << "Error: could not open file." << std::endl;
+        return 1;
+    }
+    BitcoinExchange bitcoin;
+    bitcoin.processInput(av[1]);
+    return 0;
+}
+/*
+2011-01-03 => 3 = 0.9
+2011-01-03 => 2 = 0.6
+2011-01-03 => 1 = 0.3
+2011-01-03 => 1.2 = 0.36
+2011-01-09 => 1 = 0.32
+Error: not a positive number.
+Error: bad input => 2001-42-42
+2012-01-11 => 1 = 7.1
+Error: too large a number.
+
+*/
