091caefcbde18dfecd05f697502a07e7b123deaf
alikosaca <alikosaca0@gmail.com>
2026-05-30 21:13:27 +0300

    feat: add BitcoinExchange class definition with input processing method

---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,24 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <string>
+
+class BitcoinExchange {
+    private:
+        float price;
+        int date;
+        int month;
+        int year;
+    public:
+        //BitcoinExchange();
+        //BitcoinExchange(const BitcoinExchange& other);
+        //BitcoinExchange& operator=(const BitcoinExchange& other);
+        //~BitcoinExchange();
+
+        void processInput(std::string Input);
+        //void InputIsValid(); //hata kontrolleri
+        //void Isnegative();
+};
+
+#endif 
