4a3e26593df716c40c23d63a71cfd26eff535523
alikosaca <alikosaca0@gmail.com>
2026-06-26 18:21:11 +0300

    feat: add input validation and error handling in main function of PmergeMe

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -4,7 +4,15 @@
 
 int main(int ac, char** av)
 {
-    PmergeMe merge;
-    merge.Run(ac, av);
+    if (ac < 2){
+        std::cout << "Usage: ./PmergeMe numbers" << std::endl;
+        return 2;
+    }
+    try {
+        PmergeMe merge;
+        merge.Run(ac, av);
+    } catch(std::exception& e){
+        std::cout << e.what() << std::endl;
+    }
     return (0);
 }
