1f7e25642f706f8b30cb65b1a4cde752e06d269c
ali <137689491+alikosaca@users.noreply.github.com>
2026-09-24 20:46:49 +0000

    fix: correct class name and redirect error messages to std::cerr

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include "Rpn.hpp"
+#include "RPN.hpp"
 
 int main(int ac, char** av)
 {
     if (ac != 2){
-        std::cout << "Error: could not open file." << std::endl;
+        std::cerr << "Error: could not open file." << std::endl;
         return 1;
     }
-    Rpn rpn;
+    RPN rpn;
     try{
         rpn.Run(av[1]);
     } catch(std::exception& e){
