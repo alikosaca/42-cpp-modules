c92fd2f473beb32436e8921bed2f9d1e59f1fd0c
ali <137689491+alikosaca@users.noreply.github.com>
2026-09-24 14:28:05 +0000

    fix: improve date validation in DateIsValid function

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -18,9 +18,9 @@ BitcoinExchange::~BitcoinExchange(){}
 std::string BitcoinExchange::DateIsValid(std::string line){   
     if (line.length() == 0) return ("null");
     if (line.length() < 14) throw "bad input => "+ line;
-    std::string date = line.substr(0, 13);
-    if (date[4] != '-' || date[7] != '-') throw "file in invalid format";
-    if (date[10] != ' ' || date[11] != '|' || date[12] != ' ') throw "file in invalid format";
+    if (line[4] != '-' || line[7] != '-') throw "file in invalid format";
+    if (line[10] != ' ' || line[11] != '|' || line[12] != ' ') throw "file in invalid format";
+    std::string date = line.substr(0, 10);
     int year = std::atoi(date.substr(0, 4).c_str());
     int month = std::atoi(date.substr(5, 2).c_str());
     int day = std::atoi(date.substr(8, 2).c_str());
