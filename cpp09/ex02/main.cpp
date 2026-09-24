877f6aff34c248324a0dfaa728644cca33934b51
ali <137689491+alikosaca@users.noreply.github.com>
2026-09-24 23:02:33 +0000

    fix: redirect usage and error messages to std::cerr for consistency

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -5,14 +5,14 @@
 int main(int ac, char** av)
 {
     if (ac < 2){
-        std::cout << "Usage: ./PmergeMe numbers" << std::endl;
+        std::cerr << "Usage: ./PmergeMe numbers" << std::endl;
         return 2;
     }
     try {
         PmergeMe merge;
         merge.Run(ac, av);
     } catch(std::exception& e){
-        std::cout << e.what() << std::endl;
+        std::cerr << e.what() << std::endl;
     }
     return (0);
 }
